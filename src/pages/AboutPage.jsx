f045f51af353b63cede0aa4a27e2ff6c49a282f9
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2022-08-02 21:16:26 -0600

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -7,9 +7,10 @@ function AboutPage(props) {
     return (
         <Card>
             <div className='about'>
-                <h1>About This Project</h1>
-                <p>This is a React app to leave feedback for a product or service</p>
+                <h1>About This Application</h1>
+                <p>A simple ReactJS application to add feedback for a product or a service.</p>
                 <p>Version: 1.0.0</p>
+                <p>&#169; Cody Fingerson</p>
                 <p>
                     <Link to='/'>Back To Home</Link>
                 </p>
