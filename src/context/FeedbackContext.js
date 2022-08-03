d86eb1be9c2dec3782d7e497469e71945236515d
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2022-08-02 21:16:19 -0600

    Update FeedbackContext.js

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from "react";
-import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
@@ -18,29 +17,49 @@ export const FeedbackProvider = ({ children }) => {
 
     //Fetch feedback
     const fetchFeedback = async () => {
-        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`);
+        const response = await fetch(`/feedback?_sort=id&_order=desc`);
         const data = await response.json();
         setFeedback(data);
         setIsLoading(false);
     }
 
     // Add feedback
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4();
-        setFeedback([newFeedback, ...feedback])
+    const addFeedback = async(newFeedback) => {
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newFeedback),
+        })
+
+        const data = await response.json()
+
+        setFeedback([data, ...feedback])
     }
 
     // Delete feedback
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async(id) => {
         if (window.confirm('Are you sure you want to delete?')) {
+            await fetch(`/feedback/${id}`, {method: 'DELETE'})
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
 
     // Update feedback item
-    const updateFeedback = (id, updItem) => {
+    const updateFeedback = async (id, updItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updItem)
+        });
+
+        const data = await response.json();
+
         setFeedback(
-            feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+            feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
         )
     }
 
